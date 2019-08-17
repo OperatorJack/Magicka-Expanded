737edabf294bf24ee2f048e260b84c61d0f162a9
OperatorJack <thepaladinof@gmail.com>
2019-08-16 21:56:47 -0400

    Added new in-progress mechanic to darkness effect.

--- Magicka Expanded - Lore Friendly Pack/MWSE/mods/OperatorJack/MagickaExpanded-LoreFriendlyPack/effects/darknessEffect.lua
+++ Magicka Expanded - Lore Friendly Pack/MWSE/mods/OperatorJack/MagickaExpanded-LoreFriendlyPack/effects/darknessEffect.lua
@@ -2,10 +2,22 @@ local framework = include("OperatorJack.MagickaExpanded.magickaExpanded")
 
 tes3.claimSpellEffectId("darkness", 263)
 
+local function getActorsNearMist(cell, position)
+    local actors = {}
+    for ref in cell:iterateReferences(tes3.objectType.mobileActor) do
+        framework.debug('Reference: ' .. ref.id)
+        if (math.abs(position:distance(ref.position)) <= 150) then
+            framework.debug('Found Proximity Reference: ' .. ref.id)
+            table.insert(actors, ref)
+        end
+    end
+    return actors
+end
+
 local function onDarknessCollision(e)
     if e.collision then
         ---@type tes3magicEffect
-        local effect = framework.functions.getEffectFromEffectOnCollisionEvent(e, tes3.effect.darkness)
+        local effect = framework.functions.getEffectFromEffectOnEffectEvent(e, tes3.effect.darkness)
 
         local caster = e.sourceInstance.caster
         local effectDuration = effect.duration
@@ -16,6 +28,23 @@ local function onDarknessCollision(e)
             object = "OJ_ME_DarknessMist",
             position = mistPosition,
             cell = caster.cell
+        })
+
+        framework.debug('Starting timers.')
+        timer.start({ 
+            duration = 1,
+            callback = function()
+                framework.debug('Callback!')
+                local actors = getActorsNearMist(caster.cell, mistReference.position)
+        
+                for _, actor in pairs(actors) do
+                    tes3.removeEffects({
+                        reference = actor,
+                        effect = tes3.effect.light
+                    })
+                end
+            end, 
+            iterations = (effectDuration - 1) 
         })
 
         timer.start(
