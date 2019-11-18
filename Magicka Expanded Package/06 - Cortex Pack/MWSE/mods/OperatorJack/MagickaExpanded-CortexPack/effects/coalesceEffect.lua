8e52a7a6165f26e474c9049e82b60aa470456e58
OperatorJack <thepaladinof@gmail.com>
2019-11-18 14:22:39 -0500

    Added basic coalesce implementation.

--- Magicka Expanded Package/06 - Cortex Pack/MWSE/mods/OperatorJack/MagickaExpanded-CortexPack/effects/coalesceEffect.lua
+++ Magicka Expanded Package/06 - Cortex Pack/MWSE/mods/OperatorJack/MagickaExpanded-CortexPack/effects/coalesceEffect.lua
@@ -2,12 +2,40 @@ local framework = include("OperatorJack.MagickaExpanded.magickaExpanded")
 
 tes3.claimSpellEffectId("coalesce", 333)
 
-local function onCoalesceTick(e) 
-    framework.debug("Ticking...")
-    if (e.sourceInstance.projectile) then
-        e.sourceInstance.projectile.position = e.sourceInstance.projectile.position + tes3.getPlayerEyeVector() * 250
-    end
+local function onMobileActivated(e)
+    local mobile = e.mobile -- Added cached version here.
+    local spellInstance = mobile.spellInstance
+    local firingRef = mobile.firingMobile and mobile.firingMobile.reference
+    local caster = spellInstance and spellInstance.caster
+	mwse.log("Created mobile of '%s' that was fired by '%s' and cast by '%s'", mobile.reference, firingRef, caster)
+	
+	local isProjectileUsingCoalesce
+	local effect
+	for i = 0, 6 do
+		if (spellInstance.sourceEffects[i].id == tes3.effects.coalesce) then
+			isProjectileUsingCoalesce = true
+			effect = spellInstance.sourceEffects[i]
+		end
+	end
+
+	if (isProjectileUsingCoalesce) then
+		local spellDuration = effect.duration
+		local duration = .01
+		local interations = spellDuration / duration
+		local projectileTimer = timer.start({
+			iterations = iterations
+			duration = duration
+			callback = function()
+				if (mobile) then
+					mobile.position = mobile.position + tes3.getPlayerEyeVector() * 250
+				else
+					projectileTimer:cancel()
+				end
+			end
+		})
+	end
 end
+event.register("mobileActivated", onMobileActivated)
 
 local function Coalesce()
 	framework.effects.alteration.createBasicEffect({
@@ -29,7 +57,7 @@ local function Coalesce()
         lighting = { 0, 0, 0 },
 
 		-- Required callbacks.
-		onTick = onCoalesceTick,
+		onTick = function() e:trigger() end,
 	})
 end
 
